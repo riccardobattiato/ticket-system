916f6e5fd6fefbf5edf9fb5c6a29a941aa9af97c
riccardobattiato <riccardobattiato@proton.me>
2025-05-08 23:50:41 +0200

    feat(ui): site header title varies according to page

--- src/app/tickets/layout.tsx
+++ src/app/tickets/layout.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useMemo } from "react";
 import { AppSidebar } from "./sidebar";
 import { SiteHeader } from "./header";
+import { usePathname } from "next/navigation";
 
 export default function TicketsLayout(pageProps: PropsWithChildren) {
+  const pathname = usePathname();
+
+  const title = useMemo(() => {
+    if (pathname === "/tickets") return "Tickets";
+    if (pathname === "/tickets/new") return "Create new ticket";
+    if (pathname === "/tickets/users") return "Users list";
+    if (pathname.startsWith("/tickets/details/")) return "Ticket details";
+    return "Tickets";
+  }, [pathname]);
+
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <SiteHeader title="Tickets" />
+        <SiteHeader title={title} />
         <div className="p-4 lg:p-6">{pageProps.children}</div>
       </SidebarInset>
     </SidebarProvider>
